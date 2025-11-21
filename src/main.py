6b844915dcff732b7763af35f78eccb200a1b3f7
Andrws <rouzen2001@gmail.com>
2025-11-20 21:07:02 -0500

    pruebas en main y test

--- src/main.py
+++ src/main.py
@@ -7,13 +7,14 @@ def validar_correo (email: str) -> bool:
    # retornar True si es valido, false si no.
 
    
-   patron = r'^[a-zAzZ0-9_.+-]+@[a-zA-Z0-9-]+\. [a-zA-Z0-9-.]+$'
-   return re.match (patron,email) is not None
+   patron = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+   return re.match(patron, email) is not None
 
+if __name__== "__main__":
 
-  if __name__ == "__main__":
-   
-    prueba= [" prueba@gmail.com", "mal_correo", "otro@gmail.org" ]
+    prueba= ["prueba@gmail.com","mal_correo", "otro@gmail.org", "correoprueba2@@gmail.com"]
     for correo in prueba:
+          print(f"{correo}: {'Valido' if validar_correo(correo) else 'Invalido'}")
 
-        print (f"{correo}: {'Valido'if validar_correo(correo) else 'Invalido'}")
+
+          
